fa0a9c8424503ad1761446055de8dc8116a2c8fe
NghiaLT <lamtrong.nghia1@vietcredit.com.vn>
2024-11-04 18:08:21 +0700

    edit code next to clip 15

--- js/22810209.js
+++ js/22810209.js
@@ -26,3 +26,71 @@ async function getAuthenticate(username, password) {
 
     throw new Error(data.message);
 }
+
+async function login(e) {
+    e.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    try {
+        let token = await getAuthenticate(username, password);
+        if (token) {
+            localStorage.setItem('token', token);
+            document.getElementsByClassName("btn-close")[0].click();
+            displayControls();
+        }
+    } catch (error) {
+        document.getElementById("errorMessage").innerHTML = error;
+        displayControls(false);
+    }
+}
+
+function displayControls(isLogin = true) {
+    let linkLogins = document.getElementsByClassName("linkLogin");
+    let linkLogouts = document.getElementsByClassName("linkLogout");
+
+    let displayLogin = 'none';
+    let displayLogout = 'block';
+
+    if (!isLogin) {
+        displayLogin = 'block';
+        displayLogout = 'none';
+    }
+
+    for (let i = 0; i < 2; i++) {
+        linkLogins[i].style.display = displayLogin;
+        linkLogouts[i].style.display = displayLogout;
+    }
+}
+
+async function checkLogin() {
+    let isLogin = await verifyToken();
+    displayControls(isLogin);
+}
+
+async function verifyToken() {
+    let token = localStorage.getItem('token');
+    if (token) {
+        let response = await fetch(`${API}/users/verify`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + token
+            },
+            body: JSON.stringify({username, password})
+        });
+
+        if (response.status == 200) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function logout() {
+    localStorage.clear();
+    displayControls(false);
+}
